a41fa79f7f6282cdf97ca39c40684e47eb5120d8
sightsxxr <86920414+sightsxxr@users.noreply.github.com>
2021-08-19 01:49:53 +0300

    fixed slight bug with legend selection

--- brawlhalla_bot.py
+++ brawlhalla_bot.py
@@ -13,16 +13,22 @@ movement_directions = [keyboard.Key.left, keyboard.Key.right, keyboard.Key.up, k
 
 def press_key():
     key_probability = randint(0, 100)
-    if key_probability < 40:
+    if key_probability < 60:
         my_keyboard.press("c")
         my_keyboard.release("c")
-    elif key_probability < 70 and key_probability > 40:
+    elif key_probability < 80 and key_probability > 60:
         my_keyboard.press("x")
         my_keyboard.release("x")
+        time.sleep(uniform(0, 0.1))
         my_keyboard.press("c")
         my_keyboard.release("c")
     else:
         movement_index = randint(0, 3)
+
+        my_keyboard.press("c")
+        my_keyboard.release("c")
+
+        time.sleep(uniform(0, 0.2))
 
         my_keyboard.press("c")
         my_keyboard.release("c")
